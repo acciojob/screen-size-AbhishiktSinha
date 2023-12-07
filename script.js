fa076e951d8b9f8cb2e91a8dc5a2fcf2b00894a7
AbhishiktSinha <133574794+AbhishiktSinha@users.noreply.github.com>
2023-12-07 13:11:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const dimensionsDisplay = document.querySelector("#sizeInfo > h1");
+
+window.addEventListener("resize", (event)=> {
+    const height = window.innerHeight;
+    const width = window.innerWidth;
+
+    dimensionsDisplay.textContent = `Width: ${width} and Height: ${height}`;
+})
